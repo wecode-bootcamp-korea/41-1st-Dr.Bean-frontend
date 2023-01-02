ac8f7e7af597677cd0cd64dae97dde4dc9f39fcc
김기윤 <ki@Kis-MacBook-Pro.local>
2023-01-02 20:45:25 +0900

    Add: Nav & components

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import Main from "./pages/Main/Main";
+import Login from "./pages/Login/Login";
+import Product from "./pages/ProductList/Product/Product";
+import ProductList from "./pages/ProductList/ProductList";
+import ProductDetail from "./pages/ProductDetail/ProductDetail";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Nav />} />
+        <Route path="/Main" element={<Main />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Product" element={<Product />} />
+        <Route path="/ProductList" element={<ProductList />} />
+        <Route path="/ProductDetail" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
   );
