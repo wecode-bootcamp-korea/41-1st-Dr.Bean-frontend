9974d50bb64844ee70888c858622de188d379de4
이진경 <ijinkyung12@gmail.com>
2023-01-03 17:37:04 +0900

    Modify : 메인 레이아웃 구현중

--- src/pages/Main/MainScroll/MainScroll.jsx
+++ src/pages/Main/MainScroll/MainScroll.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./MainScroll.scss";
+import { BsChevronLeft } from "react-";
 
 function MainScroll() {
   return (
     <div className="scroll-container">
       <div>
         <div className="btn-wrap inner">
-          <i class="fa-solid fa-angle-left" />
-          <i class="fa-solid fa-angle-right" />
+          {/* <i class="fa-solid fa-angle-left" />
+          <i class="fa-solid fa-angle-right" /> */}
+          {/* <FontAwesomeIcon
+            icon={faChevronLeft}
+            size="10px"
+            className="left-btn"
+          />
+          <FontAwesomeIcon icon={faChevronRight} className="right-btn" /> */}
+          {/* BsChevronLeft */}
         </div>
       </div>
     </div>
