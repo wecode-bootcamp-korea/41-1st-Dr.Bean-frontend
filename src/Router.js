3f9a9c36f55470272608e1a4ef0e5cd2adf7ad54
박채연 <cccdus313@gmail.com>
2023-01-03 22:28:26 +0900

    Add: 결제페이지 레이아웃 구성

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Payment from "./pages/Payment/Payment";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Payment />} />
       </Routes>
     </BrowserRouter>
   );
