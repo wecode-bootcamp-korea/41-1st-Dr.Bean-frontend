e89f89795f1218622eff30d060bfb2b1b9cab6f0
이진경 <ijinkyung12@gmail.com>
2023-01-03 15:43:25 +0900

    Modify : 상품 나열 레이아웃 구현 중

--- src/pages/Main/MainFilter/MainFilterItem/MainFilterItem.jsx
+++ src/pages/Main/MainFilter/MainFilterItem/MainFilterItem.jsx
@@ -2,22 +2,33 @@ import React from "react";
 import "./MainFilterItem.scss";
 
 function MainFilterItem() {
-  // const ITEM_DATA = [{
-  //   id:1,
-  //   img: ,
-  //   name : 상품이름,
-  //   price : 260,000
-
-  // }];
+  const ITEM_DATA = [
+    {
+      id: 1,
+      img: "/images/coffee_bean.jpg",
+      name: "상품이름",
+      price: "260,000",
+    },
+    {
+      id: 2,
+      img: "/images/coffee_bean.jpg",
+      name: "상품이름",
+      price: "260,000",
+    },
+    {
+      id: 3,
+      img: "/images/coffee_bean.jpg",
+      name: "상품이름",
+      price: "260,000",
+    },
+  ];
   return (
-    <>
-      <ul className="item-container inner">
-        <li className="item">
-          <img className="item-img" />
-          <p>상품이름</p>
-        </li>
-      </ul>
-    </>
+    <ul className="item-container inner">
+      <li className="item">
+        <img className="item-img" />
+        <p class="fa-solid fa-won-sign">상품이름</p>
+      </li>
+    </ul>
   );
 }
 
