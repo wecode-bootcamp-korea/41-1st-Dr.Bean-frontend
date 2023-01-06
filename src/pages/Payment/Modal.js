9e827be1ae05c525d2caeba71a9fa7d87f69b8ca
박채연 <cccdus313@gmail.com>
2023-01-06 20:34:10 +0900

    .

---
+++ src/pages/Payment/Modal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Modal.scss";
+
+const Modal = () => {
+  return (
+    <div className="Header">
+      <h2>주문상품</h2>
+    </div>
+  );
+};
+
+export default Modal;
