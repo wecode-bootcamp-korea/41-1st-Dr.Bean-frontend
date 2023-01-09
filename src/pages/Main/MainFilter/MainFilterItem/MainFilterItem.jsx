38ffda75de586ae8188dbc59633280acd28702ec
이진경 <ijinkyung12@gmail.com>
2023-01-09 13:18:43 +0900

    Modify : 메인 기능 수정 중

--- src/pages/Main/MainFilter/MainFilterItem/MainFilterItem.jsx
+++ src/pages/Main/MainFilter/MainFilterItem/MainFilterItem.jsx
@@ -2,31 +2,24 @@ import React, { useEffect, useState } from "react";
 import "./MainFilterItem.scss";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
-function MainFilterItem(activeId, setActiveId) {
+function MainFilterItem() {
   const [item, setItem] = useState([]);
   const [slidePx, setSlidePx] = useState(0);
   const widthPx = 1140;
 
-  // useEffect(() => {
-  //   fetch(`http://10.58.52.120:3000/items/1`)
-  //     .then(res => res.json())
-  //     .then(result => setItem(result));
-  // }, []);
+  useEffect(() => {
+    fetch("http://10.58.52.150:3000/items/1")
+      .then(res => res.json())
+      .then(result => setItem(result));
+  }, []);
 
   console.log(item);
 
-  const obj = {
-    0: 1,
-    1: 2,
-    2: 3,
-    3: 4,
-  };
-
-  useEffect(() => {
-    fetch("/data/FilterData.json")
-      .then(res => res.json())
-      .then(data => setItem(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/data/FilterData.json")
+  //     .then(res => res.json())
+  //     .then(data => setItem(data));
+  // }, []);
 
   const toPrev = () => {
     slidePx < 0 && setSlidePx(slidePx + widthPx);
@@ -52,7 +45,7 @@ function MainFilterItem(activeId, setActiveId) {
                 <img className="item-img" src={str.item_img} />
                 <div className="item-sub-wrap">
                   <p class="fa-solid fa-won-sign">{str.name}</p>
-                  <p>{str.price}</p>
+                  <p className="product-price">{str.price}</p>
                 </div>
               </li>
             );
