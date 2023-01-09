2666aa4d6cf84d3e1fba21dd891134661ca1d10e
이진경 <ijinkyung12@gmail.com>
2023-01-09 11:02:17 +0900

    Add : 브랜드 페이지 추가

--- src/Router.js
+++ src/Router.js
@@ -7,6 +7,7 @@ import Login from "./pages/Login/Login";
 import Product from "./pages/ProductList/Product/Product";
 import ProductList from "./pages/ProductList/ProductList";
 import ProductDetail from "./pages/ProductDetail/ProductDetail";
+import BrandStory from "./pages/BrandStory/BrandStory";
 
 export default function Router() {
   return (
@@ -19,6 +20,7 @@ export default function Router() {
         <Route path="/product" element={<Product />} />
         <Route path="/productList" element={<ProductList />} />
         <Route path="/productDetail" element={<ProductDetail />} />
+        <Route path="/brandStory" element={<BrandStory />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/BrandStory/BrandStory.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../BrandStory/BrandStory.scss";
+
+function BrandStory() {
+  return <div className="brand-story-container inner" />;
+}
+
+export default BrandStory;
