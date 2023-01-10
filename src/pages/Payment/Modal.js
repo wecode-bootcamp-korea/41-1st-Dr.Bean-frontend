43a047fff45e06e64c3f408cc08feb0c9c5e4225
박채연 <cccdus313@gmail.com>
2023-01-10 10:50:53 +0900

    기능수정중

--- src/pages/Payment/Modal.js
+++ src/pages/Payment/Modal.js
@@ -1,30 +1,20 @@
 import "./Modal.scss";
 
 const Modal = props => {
+  // const [paymentList, setPaymentList] = [];
+
   return (
     <div className="modal" hidden={props.hidden}>
       <div className="modal-img">
         <div className="item-info">
-          <h2>2976 첼시 스무스</h2>
-          <div className="option">
-            <select className="size">
-              <option>100g</option>
-              <option>500g</option>
-              <option>1kg</option>
-            </select>
-            <select className="grind">
-              <option>(분쇄)에스프레소</option>
-              <option>(분쇄)커피메이커</option>
-              <option>(분쇄)모카포토</option>
-              <option>(분쇄)더치커피</option>
-              <option>(분쇄)프렌치프레소</option>
-              <option>(분쇄)에어로프레스</option>
-              <option>(홀빈)갈비않은 원두</option>
-            </select>
+          <h2>{}</h2>
+          <div className="{option}">
+            <div className="{size}" />
+            <div className="{grind}" />
           </div>
 
           <div className="info-price">
-            <p className="price">₩260,000{}</p>
+            <p className="price">{}</p>
           </div>
         </div>
       </div>
