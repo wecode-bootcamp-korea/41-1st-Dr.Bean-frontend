435199509227004c939e6a138b5e761cda418535
박채연 <cccdus313@gmail.com>
2023-01-10 15:43:56 +0900

    데이터추가

---
+++ src/components/Nav/NavsideCountry.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export const NavsideCountry = [
+  {
+    title: "아프리카",
+    area: [
+      { name: "케냐" },
+      { name: "에티오피아" },
+      { name: "르완다" },
+      { name: "카메룬" },
+    ],
+  },
+  {
+    title: "남 아메리카",
+    area: [
+      { name: "브라질" },
+      { name: "콜롬비아" },
+      { name: "페루" },
+      { name: "에콰도르" },
+    ],
+  },
+  {
+    title: "북 중앙 아메리카",
+    area: [
+      { name: "파나마" },
+      { name: "코스타리카" },
+      { name: "온두라스" },
+      { name: "과테말라" },
+    ],
+  },
+  {
+    title: "아시아",
+    area: [{ name: "인도네시아" }, { name: "예멘" }, { name: "베트남" }],
+  },
+];
