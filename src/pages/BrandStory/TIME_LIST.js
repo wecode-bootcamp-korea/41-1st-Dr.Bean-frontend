7b6d91e84c63dfb58c28cad7d4109f395aa82354
이진경 <ijinkyung12@gmail.com>
2023-01-10 15:21:21 +0900

    Add : 브랜드 스토리 추가

---
+++ src/pages/BrandStory/TIME_LIST.js
@@ -0,0 +1,28 @@
+const TIME_LIST = [
+  {
+    id: 1,
+    time: "8시",
+  },
+  {
+    id: 2,
+    time: "10시",
+  },
+  {
+    id: 3,
+    time: "12시",
+  },
+  {
+    id: 4,
+    time: "14시",
+  },
+  {
+    id: 5,
+    time: "16시",
+  },
+  {
+    id: 6,
+    time: "18시",
+  },
+];
+
+export default TIME_LIST;
