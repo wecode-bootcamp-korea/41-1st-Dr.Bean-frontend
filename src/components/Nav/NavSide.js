69cd3404249bbeb400bfaf3f90371157e040146e
박채연 <cccdus313@gmail.com>
2023-01-11 16:02:23 +0900

    사이드바 url

--- src/components/Nav/NavSide.js
+++ src/components/Nav/NavSide.js
@@ -1,15 +1,7 @@
 import React, { useState } from "react";
 import "./NavSide.scss";
-
-const sideArray = [
-  { title: "아프리카", area: ["케냐", "에티오피아", "르완다", "카메룬"] },
-  { title: "남 아메리카", area: ["브라질", "콜롬비아", "페루", "에콰도르"] },
-  {
-    title: "북 중앙 아메리카",
-    area: ["파나마", "코스타리카", "온두라스", "과테말라"],
-  },
-  { title: "아시아", area: ["인도네시아", "예맨", "베트남"] },
-];
+import { sideArray } from "./NavArea";
+import { Navigate, useNavigate } from "react-router-dom";
 
 let result = [];
 const NavSide = props => {
@@ -28,6 +20,7 @@ const NavSide = props => {
 
     setSubMenu({ list: result, title: value });
   };
+  const navigate = useNavigate();
 
   return (
     <div className="NavSide" onClick={() => props.onClose()}>
@@ -43,7 +36,16 @@ const NavSide = props => {
             <div className="sub-menu">
               {item.title === subMenu.title ? (
                 subMenu.list.map((item, index) => {
-                  return <div key={index}>{item}</div>;
+                  return (
+                    <div
+                      key={index}
+                      onClick={e => {
+                        navigate(item.url);
+                      }}
+                    >
+                      {item.name}
+                    </div>
+                  );
                 })
               ) : (
                 <div />
