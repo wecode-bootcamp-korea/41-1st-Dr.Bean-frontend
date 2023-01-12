fd2761959b21deea7c2e8d40e6a965bba42445d4
이진경 <ijinkyung12@gmail.com>
2023-01-12 13:42:59 +0900

    Modify : 사이드바 수정

--- src/components/Nav/NavSide.js
+++ src/components/Nav/NavSide.js
@@ -2,14 +2,16 @@ import React, { useState } from "react";
 import "./NavSide.scss";
 import { sideArray } from "./NavArea";
 import { Navigate, useNavigate } from "react-router-dom";
+import { BsChevronDown } from "react-icons/bs";
 
 let result = [];
 const NavSide = props => {
   const [subMenu, setSubMenu] = useState({ list: [], title: "" });
+  const [view, setView] = useState(false);
 
   const navClick = (value, event) => {
     event.stopPropagation();
-
+    setView(!view);
     result = [];
     sideArray.forEach(function (item) {
       if (item.title === value) {
@@ -20,37 +22,42 @@ const NavSide = props => {
 
     setSubMenu({ list: result, title: value });
   };
+
   const navigate = useNavigate();
 
   return (
     <div className="NavSide" onClick={() => props.onClose()}>
       {sideArray.map((item, index) => {
         return (
-          <div key={index} className="side-menu">
-            <div
-              className="side-list"
-              onClick={event => navClick(item.title, event)}
-            >
-              {item.title}
-            </div>
-            <div className="sub-menu">
-              {item.title === subMenu.title ? (
-                subMenu.list.map((item, index) => {
-                  return (
-                    <div
-                      key={index}
-                      onClick={e => {
-                        navigate(item.url);
-                      }}
-                    >
-                      {item.name}
-                    </div>
-                  );
-                })
-              ) : (
-                <div />
-              )}
-            </div>
+          <div
+            key={index}
+            className="side-menu"
+            onClick={event => navClick(item.title, event)}
+          >
+            {item.title}
+            <BsChevronDown className="down-arrow" />
+
+            {view && (
+              <div className="sub-menu">
+                {item.title === subMenu.title ? (
+                  subMenu.list.map((item, index) => {
+                    return (
+                      <div
+                        key={index}
+                        onClick={e => {
+                          navigate(item.url);
+                        }}
+                        className="country-items"
+                      >
+                        {item.name}
+                      </div>
+                    );
+                  })
+                ) : (
+                  <div />
+                )}
+              </div>
+            )}
           </div>
         );
       })}
